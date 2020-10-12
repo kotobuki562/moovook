38725daac253f4e34094c5c38ce7dd39156b3a3a
MaQueen <shusineko@icloud.com>
2020-10-12 14:28:47 +0900

    active_hash設定

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,29 @@
-class Category < ApplicationRecord
+class Category < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '---' },
+    { id: 2, name: '文学・評論' },
+    { id: 3, name: '人文・思想' },
+    { id: 4, name: '社会・政治' },
+    { id: 5, name: 'ノンフィクション' },
+    { id: 6, name: '歴史・地理' },
+    { id: 7, name: 'ビジネス・経済' },
+    { id: 8, name: '投資・金融・会社経営' },
+    { id: 9, name: '自己啓発' },
+    { id: 10, name: '科学・テクノロジー' },
+    { id: 11, name: 'アート・建築・デザイン' },
+    { id: 13, name: '医学・薬学・看護学・歯科学' },
+    { id: 14, name: 'コンピュータ・IT' },
+    { id: 15, name: '実用' },
+    { id: 16, name: '趣味' },
+    { id: 17, name: 'スポーツ' },
+    { id: 18, name: 'アウトドア' },
+    { id: 19, name: '資格検定' },
+    { id: 20, name: '暮らし・健康・子育て' },
+    { id: 21, name: '旅行・レジャー' },
+    { id: 22, name: '語学' },
+    { id: 23, name: '教育・学参・受験' },
+    { id: 24, name: 'エンターテイメント' },
+    { id: 25, name: 'コミック・ラノベ' },
+    { id: 26, name: 'その他' }
+  ]
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,17 @@
 class Post < ApplicationRecord
-  belongs_to :user
-  has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
 
   with_options presence: true do
     validates :image
     validates :book_name
-    validates :category_id
+    validates :category_id, numericality: { other_than: 1 }
     validates :wrap_up
     validates :impressions
     validates :action_plan
   end
+
+  belongs_to :user
+  has_one_attached :image
+  
 end
