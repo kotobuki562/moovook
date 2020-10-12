98c6ae1fecfe01cf0982f455b7ebf459914ca9d3
MaQueen <shusineko@icloud.com>
2020-10-12 12:36:43 +0900

    active_storageの導入とpostsモデルにhas_one_attached imageを設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,12 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :book_name
+    validates :category_id
+    validates :wrap_up
+    validates :impressions
+    validates :action_plan
+  end
 end
