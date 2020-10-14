9d7a9f008ff5b86b75c30b4d935f5c54cbe3fc82
MaQueen <shusineko@icloud.com>
2020-10-14 11:10:09 +0900

    commentコントローラーとルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'posts#index'
   resources :users, only: [:show]
-  resources :posts, only: [:new, :create, :show, :edit, :update, :destroy]
+  resources :posts, only: [:new, :create, :show, :edit, :update, :destroy] do
+    resources :comments, only: [:create]
+  end
 end
