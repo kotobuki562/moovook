8fd97763ea563aa67d6f5036fdea60582efbcd4e
MaQueen <shusineko@icloud.com>
2020-10-14 17:17:00 +0900

    いいね機能実装2

--- app/models/post.rb
+++ app/models/post.rb
@@ -15,8 +15,7 @@ class Post < ApplicationRecord
   end
 
   belongs_to :user
-  has_many :likes
-  has_many :comments
+  has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
   has_one_attached :image
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,17 +6,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  with_options presence: true do
-    validates :password, format: { with: VALID_PASSWORD_REGEX }
-    validates :name, length: { maximum: 10 }
+with_options presence: true do
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
+  validates :name, length: { maximum: 10 }
+end
+
+  has_many :posts
+  has_many :comments
+  has_many :likes
+
+  def liked_by?(post_id)
+    likes.where(post_id: post_id).exists?
   end
-
-    has_many :posts
-    has_many :comments
-    has_many :likes
-
-    def liked_by?(post_id)
-      likes.where(post_id: post_id).exists?
-    end
 
 end
