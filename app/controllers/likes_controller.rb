d18984d8542c1709bdab4adb9628d75a7b8f0c24
MaQueen <shusineko@icloud.com>
2020-10-16 12:48:08 +0900

    いいねの非同期通信実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,13 +3,15 @@ class LikesController < ApplicationController
   before_action :post_params
   
   def create
+    @posts = Post.includes(:user).order('created_at DESC')
     Like.create(user_id: current_user.id, post_id: params[:id])
-    redirect_to root_path
+    # redirect_to root_path
   end
 
   def destroy
+    @posts = Post.includes(:user).order('created_at DESC')
     Like.find_by(user_id: current_user.id, post_id: params[:id]).destroy
-    redirect_to root_path
+    # redirect_to root_path
   end
 
   private
