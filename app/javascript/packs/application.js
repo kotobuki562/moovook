3f7f7ad83c3fa9efc0eb1842793cf93f5c88feb9
MaQueen <shusineko@icloud.com>
2020-10-19 18:26:35 +0900

    navbar実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 require("../comment");
 require("../darkmode")
+require("../nav")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
