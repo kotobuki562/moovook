c5fb8fbb25a6c522fa96c7664c0f66d8b3a5be5e
MaQueen <shusineko@icloud.com>
2020-10-21 11:19:53 +0900

    マイページでいいねの一覧表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = @user.posts.order('created_at DESC').page(params[:page]).per(6)
+    @likes = Like.where(user_id: @user.id)
   end
 end
