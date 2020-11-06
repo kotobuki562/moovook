b2d0ffb7e9c4415c4b97f12c1213d61d39b1f36e
MaQueen <shusineko@icloud.com>
2020-11-06 18:11:05 +0900

    下準備完了

---
+++ app/javascript/packs/Slide.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Swipe from "./Swipe";
+import "./style.css";
+function Slide() {
+  return (
+    <div className="App">
+      <h1>スマホアプリっぽいWebページ</h1>
+      <Swipe />
+    </div>
+  );
+}
+
+export default Slide;
---
+++ app/javascript/packs/slider.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import './index.css';
+import Slide from './Slide';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Slide />
+  </React.StrictMode>,
+  document.getElementById('slider')
+);
