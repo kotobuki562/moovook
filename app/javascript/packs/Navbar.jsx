cbc7818ca2e3e3f64026b8efaff0e5b8d05a91e4
MaQueen <shusineko@icloud.com>
2020-11-09 19:37:42 +0900

    commit

--- app/javascript/packs/Navbar.jsx
+++ app/javascript/packs/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useRef, useEffect } from "react";
 import { FaBars, FaTwitter } from "react-icons/fa";
 import { links, social } from "./data";
-import logo from "./logo3.svg";
+import logo from "./logo4.svg";
 
 const Navbar = () => {
   const [showLinks, setShowLinks] = useState(false);
